8d3b48c3f44b755d67dbe6661fe239fb57f07bb1
Vasanta Lim Xin Jing <vasanta97@gmail.com>
2019-03-25 22:54:50 +0800

    added postgres database configuration

--- config/database.php
+++ config/database.php
@@ -1,5 +1,7 @@
 <?php
 
+$DATABASE_URL = parse_url('postgres://jfxhttocfujaod:cf11817313e65ba9def33154a12446ed23d2e377d513d72a1a616725c0e8ff5b@ec2-54-83-61-142.compute-1.amazonaws.com:5432/d72hhiq4pdmcv7');
+
 return [
 
     /*
@@ -13,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -55,17 +57,17 @@ return [
         ],
 
         'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'prefer',
-        ],
+                    'driver' => 'pgsql',
+                    'host' => $DATABASE_URL["host"],
+                    'port' => $DATABASE_URL["port"],
+                    'database' => ltrim($DATABASE_URL["path"], "/"),
+                    'username' => $DATABASE_URL["user"],
+                    'password' => $DATABASE_URL["pass"],
+                    'charset' => 'utf8',
+                    'prefix' => '',
+                    'schema' => 'public',
+                    'sslmode' => 'require',
+                ],
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
